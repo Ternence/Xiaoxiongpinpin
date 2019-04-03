a74c819544ac1c2b51e2904ccd94d64fc9c598cc
Zeo <yangcrazy30@hotmail.com>
2019-04-03 21:47:52 +0800

    pay update

--- client/pages/readyToPay/readyToPay.js
+++ client/pages/readyToPay/readyToPay.js
@@ -1,19 +1,76 @@
 // pages/readyToPay/readyToPay.js
+var app = getApp();
+import {
+  $login,
+  $request,
+  Session
+} from '../../lib/page.auth'
+import config from '../../config'
+
+const {
+  regeneratorRuntime
+} = global
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    address:'',
-    
+    address:'请选择收获地址',
+    price:'',
+    user:'',
+    phonenumber:'',
+    time:'20:53',
+    cart:[],
+  },
+
+  choseaddress:function(){
+    wx.navigateTo({
+      url: '../address/address',
+    });
+    // var district=address.district;
+    // var detail=address.detail;
+    // console.log(district+detail);
+  },
+  setaddress:function(){
+    var address = app.globalData.currentaddress;
+    var ad = address.district + " " + address.detail;
+    if(address.detail!="")
+    {
+      this.setData({
+        address: ad,
+        user: app.globalData.userInfo.nickName,
+        phonenumber: address.phone
+      })
+    }
+    else
+    {
+      this.setData({
+        address: ad,
+        user: app.globalData.userInfo.nickName,
+      })
+    }
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var cart = JSON.parse(options.cart);
 
+    var total=0;
+    for(let i=0;i<cart.length;i++)
+    {
+      total=total+cart[i].total
+    }
+    this.setData({
+      cart: cart,
+      price:total
+    })
+    console.log(total);
+    // console.log('pay');
+    // console.log(cart);
   },
 
   /**
@@ -27,7 +84,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.setaddress();
   },
 
   /**
