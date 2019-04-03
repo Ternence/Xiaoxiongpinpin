46069f7cb9af11df4bb0ac522ea6b2372975ac9a
Zeo <yangcrazy30@hotmail.com>
2019-04-03 21:48:14 +0800

    home update

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -2,10 +2,6 @@
 var API = require('../../utils/api.js')
 var app = getApp();
 import {
-  $init,
-  $digest
-} from '../../lib/page.data'
-import {
   $login,
   $request,
   Session
@@ -36,32 +32,71 @@ Page({
     islogin: getApp().globalData.islogin,
     currentad: '',
     height: 555,
+    cart: []
   },
 
   //获取商品类别
   getCatagorys: async function(options) {
-    var res=await $request({url:config.url.catagory});
+    var res = await $request({
+      url: config.url.catagory
+    });
+    var temp = res.data.categories;
+    temp.unshift({
+      id: '',
+      name: '全部'
+    });
     this.setData({
-      catagory:res.data.catagories
+      catagory: res.data.categories
     })
   },
   //获取商品
   getGoods: async function(options) {
-    var res = await $request({ url: config.url.allgoods});
-    console.log(res);
-    // API.ajax('/fruit', function(res) {
-    //   that.setData({
-    //     goods: res.goods
-    //   })
-    // });
+    var res = await $request({
+      url: config.url.allgoods
+    });
+
+    var goods = this.nomarlizegoods(res.data.goods);
+    this.setData({
+      goods: goods
+    })
   },
-  // 动态样式
-  Click: function(event) {
+  // 格式化商品数据
+  nomarlizegoods: function(goods) {
+    goods = goods.map(value => ({
+      id: value.id,
+      name: value.name,
+      catagory: value.category,
+      stock: value.stock,
+      price: value.price,
+      status: value.status,
+      number: 0,
+      options: value.options,
+      description: value.description
+    }));
+    return goods;
+  },
+  // 选择商品种类
+  getGoodsByCategory: async function(event) {
+    var that = this;
     this.setData({
       clicknumber: event.currentTarget.dataset.gid
     })
-    this.getGoods();
-
+    if (event.currentTarget.dataset.gid == 0) {
+      this.getGoods();
+    } else {
+      var res = await $request({
+        url: config.url.goodsbycatagory,
+        data: {
+          category: event.currentTarget.dataset.category.name
+        }
+      });
+      var goods = this.nomarlizegoods(res.data.goods);
+      this.setData({
+        goods: goods
+      });
+    }
+    // console.log(this.data.goods);
+    this.setcart();
   },
   // 跳转搜索
   linkToSearch: function(options) {
@@ -83,84 +118,107 @@ Page({
       url: '../goodsDetail/goodsDetail?goods=' + JSON.stringify(goods),
     })
   },
-  // 展开选项卡
-  openOption: function() {
-    this.setData({
-      groundhidden: false,
-    })
-    this.getOptions();
-  },
-  // 获取选项
-  getOptions: function() {
-    var that = this;
-    API.ajax('/option', function(res) {
-      that.setData({
-        options: res.options
-      })
-    });
-    var temp = [];
-    var options = this.data.options;
-    var len = options.length;
-    for (let i = 0; i < len; i++) {
-      temp[i] = {
-        title: options[i].options[0].name
-      };
-    }
-    this.setData({
-      selected: temp
-    })
-  },
-  // 设置已选项
-  setSelect: function(event) {
-    var index = event.currentTarget.dataset.op;
-    var selectindex = event.detail.selectindex;
-    var param = {};
-    // console.log(index)
-    // console.log(this.data.options[index].options[selectindex])
-    var string = "selected[" + index + "].title";
-    param[string] = this.data.options[index].options[selectindex].name;
-    this.setData(param);
-    param = {};
-    var string = "options[" + index + "].selectindex";
-    param[string] = selectindex;
-    this.setData(param);
-    console.log(this.data.options)
-  },
-  // 准备添加
-  readyforadd: function(event) {
-    this.openOption()
+  // 提交购物车
+  addtoCart: async function(event) {
     var index = event.currentTarget.dataset.goodid;
     this.setData({
       index: index
     })
-  },
-  // 隐藏选项卡
-  cancelOption: function() {
-    this.setData({
-      groundhidden: true,
-    })
-  },
-  // 提交购物车
-  addtoCart: function(event) {
     var num = this.data.goods[this.data.index].number;
     var param = {};
     num = num + 1;
     var string = "goods[" + this.data.index + "].number";
     param[string] = num;
     this.setData(param);
-    this.cancelOption();
-    // TODO 更新购物车
+    this.changethecart();
+    var req = await $request({
+      url: config.url.updatecart,
+      method: 'POST',
+      data: {
+        goods: this.data.cart
+      }
+    });
+
+  },
+  changethecart: function() {
+    var goods = this.data.goods[this.data.index];
+    var cart = this.data.cart;
+    var flag = 0;
+    var that = this;
+    var num = goods.number;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].id == goods.id) {
+        cart[i].num = num;
+        flag = 1;
+      }
+    }
+    if (flag == 0) {
+      goods = {
+        id: goods.id,
+        num: goods.number,
+        name:goods.name,
+        description: goods.description,
+        options: [],
+        price:goods.price
+      };
+      cart.push(goods);
+    }
+    var remove0 = [];
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].num != 0)
+        remove0.push(cart[i])
+    }
+    this.setData({
+      cart: remove0
+    })
   },
   //  
-  removefromCart: function(event) {
+  removefromCart: async function(event) {
     var index = event.currentTarget.dataset.goodid;
+    this.setData({
+      index: index
+    })
     var num = this.data.goods[index].number;
     var param = {};
     num = num - 1;
     var string = "goods[" + index + "].number";
     param[string] = num;
     this.setData(param);
-    // TODO 更新购物车
+    this.changethecart();
+    // console.log(this.data.cart);
+    var req = await $request({
+      url: config.url.updatecart,
+      method: 'POST',
+      data: {
+        goods: this.data.cart
+      }
+    });
+  },
+  getcart: async function() {
+    var res = await $request({
+      url: config.url.getcart
+    });
+    this.setData({
+      cart: res.data
+    });
+    // console.log(this.data.cart);
+
+    this.setcart();
+  },
+  setcart: function() {
+    var goods = this.data.goods;
+    for (let i = 0; i < this.data.goods.length; i++) {
+      for (let j = 0; j < this.data.cart.length; j++) {
+        if (this.data.goods[i].id == this.data.cart[j].id) {
+          goods[i].number = this.data.cart[j].num
+        }
+      }
+    }
+    this.setData({
+      goods: goods
+    });
+    // console.log('1');
+    // console.log(this.data.goods);
   },
   cancel: function() {
     this.setData({
@@ -189,7 +247,7 @@ Page({
     }
   },
   lower: function(event) {
-    console.log('到底了')
+    // console.log('到底了')
   },
 
 
@@ -199,7 +257,7 @@ Page({
    */
   onLoad: function(options) {
     this.setData({
-      height: wx.getSystemInfoSync().windowHeight
+      height: wx.getSystemInfoSync().windowHeight,
     })
     const session = Session.get()
     if (session) {
@@ -227,11 +285,14 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
+    
     this.setData({
-      currentad: app.globalData.currentaddress
+      currentad: app.globalData.currentaddress.detail,
+      clicknumber:0
     })
     this.getCatagorys();
     this.getGoods();
+    this.getcart();
   },
 
   /**
