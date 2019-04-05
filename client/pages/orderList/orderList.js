51f1edd23f0bcbba068cfa1c80c7e829667e47ac
Zeo <yangcrazy30@hotmail.com>
2019-04-05 16:33:25 +0800

    update orderlist

--- client/pages/orderList/orderList.js
+++ client/pages/orderList/orderList.js
@@ -1,26 +1,38 @@
 // pages/orderList/orderList.js
+
+var app = getApp();
+import {
+  $login,
+  $request,
+  Session
+} from '../../lib/page.auth'
+import config from '../../config'
+
+const {
+  regeneratorRuntime
+} = global
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    title:['全部','待付款','待配送','已完成','售后退款'],
-    clickNumber:0
+    title:['全部','配送中','已完成','售后退款'],
+    clickNumber:0,
+    orders:[]
 
   },
   // 页面切换
   centerTap: function(event) {
     //点击的偏移量
     var cur = event.detail.x;
-    //每个tab选项宽度占15%
-    var singleNavWidth = wx.getSystemInfoSync().windowWidth *20 / 100;
+    var singleNavWidth = wx.getSystemInfoSync().windowWidth *25 / 100;
     this.setData({
       clickNumber: parseInt(cur / singleNavWidth)
     })
+    this.getOrder();
   },
   changeSwipe: function(event) {
-    console.log(event);
     var type =
       event.detail.current;
     this.setData({
@@ -28,14 +40,85 @@ Page({
     });
   },
   // 获取订单
-  getOrder:function(options){
-
+  getOrder:async function(options){
+    var res={};
+    var clicknumber = +this.data.clickNumber;
+    switch(clicknumber)
+    {
+      case 0:
+        console.log('0in')
+        res = await $request({ url: config.url.getorder});
+        break;
+      case 1: 
+      console.log('1in');
+        res = await $request({ url: config.url.getorder, data: { status: '配送中'} });
+        break;
+      case 2: 
+        console.log('2in');
+        res = await $request({ url: config.url.getorder, data: { status: '已送达' } });
+        break;
+      case 3: 
+        console.log('3in');
+        var temp=[];
+        res = await $request({ url: config.url.getorder, data: { status: '申请退款中' } });
+        temp=temp.concat(res.data.orders);
+        var res1 = await $request({ url: config.url.getorder, data: { status: '已退款' } });
+        temp=temp.concat(res1.data.orders);
+        res={data:{orders:temp}};
+        break; 
+    }
+    if(res.data==undefined)
+    {
+      this.setData({
+        orders:[]
+      })
+    }
+    else
+    {
+      var orders=res.data.orders;
+      Date.prototype.format = function (fmt) {
+        var o = {
+          "M+": this.getMonth() + 1,                 //月份 
+          "d+": this.getDate(),                    //日 
+          "h+": this.getHours(),                   //小时 
+          "m+": this.getMinutes(),                 //分 
+          "s+": this.getSeconds(),                 //秒 
+          "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+          "S": this.getMilliseconds()             //毫秒 
+        };
+        if (/(y+)/.test(fmt)) {
+          fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        for (var k in o) {
+          if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+          }
+        }
+        return fmt;
+      }
+      for (let i = 0; i < orders.length;i++)
+      {
+        orders[i].createTime = new Date(orders[i].createTime).format("yyyy-MM-dd hh:mm:ss");
+      }
+      this.setData({
+        orders: orders
+      });    
+    }
+  },
+  // 跳转订单详情
+  linkToOrderDetail:function(event){
+    var index=event.currentTarget.dataset.oid;
+    console.log(index);
+    wx.navigateTo({
+      url: '../orderDetail/orderDetail?order='+JSON.stringify(this.data.orders[index]),
+    })
   },
   
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    
     this.setData({
       clickNumber:options.show,
     })
@@ -52,7 +135,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.getOrder();
   },
 
   /**
