1220c475c75ffcfb70a223404d2c85b4230251e6
Zeo <yangcrazy30@hotmail.com>
2019-04-05 16:34:43 +0800

    update goodsdetail

--- client/pages/goodsDetail/goodsDetail.js
+++ client/pages/goodsDetail/goodsDetail.js
@@ -18,6 +18,8 @@ Page({
       id: 1
     },
     comment: '暂无评论',
+    comments: [],
+    cart: []
   },
 
   // 获取用户评价
@@ -32,18 +34,20 @@ Page({
     });
     if (res.data.reviews.length != 0) {
       this.setData({
-        comment: res.data.reviews[0]
+        comment: res.data.reviews[0].content,
+        comments: res.data.reviews
       })
     }
 
   },
-  // 获取展示图片
-  getImage: function() {
-
-  },
-  // 获取商品信息
-  getGoodsInfo: function() {
-
+  // 获取购物车
+  getcart: async function() {
+    var res = await $request({
+      url: config.url.getcart
+    });
+    this.setData({
+      cart: res.data
+    });
   },
   // 导航至主页
   linkToHome: function() {
@@ -57,12 +61,59 @@ Page({
       url: '../shoppingCart/shoppingCart',
     })
   },
+  // 导航至
+  linkToAllComment: function() {
+    wx.navigateTo({
+      url: '../allComment/allComment?comments=' + JSON.stringify(this.data.comments),
+    })
+  },
   // 加入购物车
-  addToCart: function(options) {
+  addToCart: async function(options) {
+    var flag = 0;
+    var goods=this.data.goods;
+    var cart=this.data.cart;
+    console.log(goods);
+    console.log(cart);
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].id == goods.id) {
+        cart[i].num = cart[i].num + 1;
+        flag = 1;
+      }
+    }
+    if (flag == 0) {
+      cart.push({
+        id: goods.id,
+        num: 1,
+        name: goods.name,
+        description: goods.description,
+        price: goods.price,
+        options: []
+      });
+    }
+    this.setData({
+      cart:cart
+    })
+    var res = await $request({
+      url: config.url.updatecart,
+      method: 'POST',
+      data: {
+        goods: cart
+      }
+    });
+    if(res.code==20000)
+    {
+      wx.showToast({
+        title: '添加成功',
+        icon: 'success',
+        duration: 1000,
+        mask: true
+      })
+    }
 
   },
   // 立即购买
   buyNow: function() {
+    // TODO
     wx.navigateTo({
       url: '../readyToPay/readyToPay',
     })
@@ -71,13 +122,15 @@ Page({
 
 
 
+
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log(options)
+    // console.log(options)
     var goods = JSON.parse(options.goods);
-    console.log(goods);
+    // console.log(goods);
     this.setData({
       goods: JSON.parse(options.goods)
     })
@@ -93,7 +146,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.getcart();
   },
 
   /**
