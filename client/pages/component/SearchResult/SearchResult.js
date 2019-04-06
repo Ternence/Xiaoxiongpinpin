4d260dc0ed66d00aa7003d8fa2cbb24e89f54af2
Zeo <yangcrazy30@hotmail.com>
2019-04-06 17:00:36 +0800

    add component sr

---
+++ client/pages/component/SearchResult/SearchResult.js
@@ -0,0 +1,23 @@
+// pages/component/SearchResult/SearchResult.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    result:Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
