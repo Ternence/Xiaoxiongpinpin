271e5467b82e531a0d3b1f2775c1d31fcfb82f32
Zeo <yangcrazy30@hotmail.com>
2019-04-12 19:35:49 +0800

    update allcomment

--- client/pages/allComment/allComment.js
+++ client/pages/allComment/allComment.js
@@ -1,4 +1,13 @@
 // pages/allComment/allComment.js
+const {
+  regeneratorRuntime
+} = global
+import {
+  $login,
+  $request,
+  Session
+} from '../../lib/page.auth'
+import config from '../../config'
 Page({
 
   /**
@@ -8,13 +17,47 @@ Page({
     comments:[{content:'很好',createdAt:'1998-02-24'}],
     height:555,
   },
+
+
+  getallcomments:async function(id){
+    var res = await $request({ url: config.url.getonecomment,data:{id:id}});
+    var reviews = res.data.reviews || [];
+    Date.prototype.format = function (fmt) {
+      var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+      };
+      if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+      }
+      for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+          fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+      }
+      return fmt;
+    }
+    reviews=reviews.map(value=>({
+      content:value.content,
+      createdAt: new Date(value.createdAt).format("yyyy-MM-dd hh:mm:ss")
+    }));
+
+    this.setData({
+      comments:reviews
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var comments=JSON.parse(options.comments);
+    var goodsid = JSON.parse(options.goodsid);
+    this.getallcomments(goodsid);
     this.setData({
-      comments:comments,
       height: wx.getSystemInfoSync().windowHeight,
     });
   },
