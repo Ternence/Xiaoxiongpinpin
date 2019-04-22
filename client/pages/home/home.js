26657d027d6a99056e33ba0b762a8d62816e5e22
Zeo <yangcrazy30@hotmail.com>
2019-04-22 12:48:01 +0800

    add loading

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -350,10 +350,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    wx.showLoading({
+      title: '加载中',
+    })
     this.getpictures();
     this.setData({
       height: wx.getSystemInfoSync().windowHeight,
     })
+    wx.hideLoading();
   },
 
   /**
