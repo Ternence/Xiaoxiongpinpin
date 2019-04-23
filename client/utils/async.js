cc362e03148e2dadad529e042b8f664468c90a67
Zeo <yangcrazy30@hotmail.com>
2019-04-23 09:22:41 +0800

    add promise

---
+++ client/utils/async.js
@@ -0,0 +1,30 @@
+Promise.prototype.finally = function (callback) {
+  let P = this.constructor;
+  return this.then(
+    value => P.resolve(callback()).then(() => value),
+    reason => P.resolve(callback()).then(() => { throw reason })
+  );
+};
+
+//封装异步api
+const wxPromisify = fn => {
+  return function (obj = {}) {
+    return new Promise((resolve, reject) => {
+      obj.success = function (res) {
+        resolve(res)
+      }
+
+      obj.fail = function (res) {
+        reject(res)
+      }
+
+      fn(obj)
+    })
+  }
+}
+const showModalPromisified = wxPromisify(wx.showModal);
+
+
+module.exports = {
+  showModalPromisified: showModalPromisified
+}
